a8259f60c8a1800211ea4bb479a4511f0f6d3a2e
Amir Akbari <amir.hossein.akbari59@gmail.com>
2026-05-15 22:20:14 +0200

    Font Hero Section updated

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -25,9 +25,15 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet" />
+      </head>
       <body
-      suppressHydrationWarning
-      className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        suppressHydrationWarning
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <AuthProvider>
           <NavbarProvider>
             <NavbarWrapper />
