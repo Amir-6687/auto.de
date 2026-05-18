8d38dceba4ce91dadd0dcbd652a0a020bc892ca8
Amir Akbari <amir.hossein.akbari59@gmail.com>
2026-05-18 23:38:23 +0200

    new font for CarCarousel

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Sora:wght@100..800&family=Rubik+Dirt&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Sora:wght@100..800&family=Rubik+Dirt&family=Alatsi&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Sora:wght@100..800&family=Rubik+Dirt&family=Alatsi&family=Quicksand:wght@300..700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Sora:wght@100..800&family=Rubik+Dirt&family=Alatsi&family=Quicksand:wght@300..700&family=Fjalla+One&display=swap" rel="stylesheet" />
       </head>
       <body
         suppressHydrationWarning
