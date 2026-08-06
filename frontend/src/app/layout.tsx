9245463c28704c323b474996048e122e4cb8ece2
Amir Akbari <amir.hossein.akbari59@gmail.com>
2026-08-06 22:53:03 +0200

    id File updated

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,56 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import NavbarWrapper from "@/components/NavbarWrapper";
+import CookieConsent from "@/components/CookieConsent";
+import AuthProvider from "@/components/AuthProvider";
+import { NavbarProvider } from "@/context/NavbarContext";
+import Footer4Col from "@/components/ui/footer-column";
+import { FavoritesProvider } from "@/context/FavoritesContext";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Auto-DE Admin",
+  description: "Car management dashboard",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Sora:wght@100..800&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Sora:wght@100..800&family=Rubik+Dirt&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Sora:wght@100..800&family=Rubik+Dirt&family=Alatsi&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Sora:wght@100..800&family=Rubik+Dirt&family=Alatsi&family=Quicksand:wght@300..700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Sora:wght@100..800&family=Rubik+Dirt&family=Alatsi&family=Quicksand:wght@300..700&family=Fjalla+One&display=swap" rel="stylesheet" />
+      </head>
+      <body
+        suppressHydrationWarning
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <AuthProvider>
+  <FavoritesProvider>
+    <NavbarProvider>
+      <NavbarWrapper />
+      <CookieConsent />
+      {children}
+      <Footer4Col />
+    </NavbarProvider>
+  </FavoritesProvider>
+</AuthProvider>
+      </body>
+    </html>
+  );
+}
